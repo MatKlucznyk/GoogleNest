ce7f1c4a8bd90261e0c498e01c3322afeda027f6
Mathew Kluczyk <mathew.klucznyk@gmail.com>
2020-09-30 22:03:24 -0400

    Initial commit

---
+++ GoogleNest/GoogleNest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+
+namespace GoogleNest
+{
+    public class Class1
+    {
+
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public Class1()
+        {
+        }
+    }
+}
---
+++ GoogleNest/GoogleNest/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("GoogleNest")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleNest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
